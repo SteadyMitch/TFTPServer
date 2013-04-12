ec210ff775a7a78b1003d23f924d1268b37addba
steadymitch <steadymitch@gmail.com>
2013-04-12 11:11:54 -0400

    tftpserv.c w/ blank functions

--- tftpserv.c
+++ tftpserv.c
@@ -1,3 +1,11 @@
+/* TO DO:
+	- Convert network address to string to write it to stdout
+	- Be able to print parsedString outside of parse function
+*/
+
+
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,11 +16,129 @@
 
 int REQUEST_LENGTH = 512;
 int tftpPort = 69;
+int errorCode = 5;
+
+/* int sendWRQ
+*/
+
+/* int sendDATA
+/
+
+/* int sendACK
+*/
+
+/*
+int sendERROR(int sockfd, char *sendbuf, size_t buflen, struct sockaddr* cliaddr)
+{
+	int bufferCount = 0;
+	const int zero = 0;
+	const int one = 1;
+	const int five = 5;
+	char FileNotFound[] = "File Not Found";
+	
+	memcpy( &sendbuf[bufferCount], &zero, sizeof(zero));
+	bufferCount++;
+	memcpy( &sendbuf[bufferCount], &five, sizeof(five));
+	bufferCount++;
+	memcpy( &sendbuf[bufferCount], &zero, sizeof(zero));
+	bufferCount++;
+	memcpy( &sendbuf[bufferCount], &one, sizeof(one));
+	bufferCount++;
+	memcpy( &sendbuf[bufferCount], FileNotFound, sizeof(FileNotFound));
+	bufferCount += strlen(FileNotFound);
+	memcpy( &sendbuf[bufferCount], &zero, sizeof(zero));
+
+	// Write error message to STDOUT
+	write(1, sendbuf, sizeof(sendbuf));
+
+	if( (sendto(sockfd, sendbuf, sizeof(sendbuf), 0, cliaddr, sizeof(cliaddr))) < 0 )
+		perror("Error Sending Error Packet");
+	return 0;
+}
+*/
+	
+/*
+void ParseRequest(char *recvmesg, int numBytesReceived, struct sockaddr_in cliaddr, char *parsedmesg)
+{
+	char RRQ[4] = "RRQ ";
+	char *filename;
+	char *mode;
+	char *clientIP;
+	char from[5] = "from ";
+	char space[] = " "; 
+	char writeChar;
+	int parsecount = 3;
+	int recvcount;
+
+	// Check for RRQ
+
+	if ((recvmesg[0] == 0) && (recvmesg[1] == 1))
+		memcpy(parsedmesg, RRQ, sizeof(RRQ));
+	else
+		perror("Not RRQ packet");
+
+	//write(1, parsedmesg, strlen(parsedmesg));
+
+	memcpy(&parsedmesg[parsecount], space, sizeof(space));
+
+	// Get FileName
+		parsecount++;
+		recvcount = 2;
+		
+		while (1)
+		{
+			if ( recvmesg[recvcount] == 0 )
+				{parsecount++; recvcount++; break;}
+
+			parsedmesg[parsecount] = recvmesg[recvcount];
+			parsecount++;
+			recvcount++;
+		}
+
+
+		write(1, parsedmesg, sizeof(parsedmesg));
+		
+		memcpy(&parsedmesg[parsecount], space, sizeof(space));
+		parsecount++;
+
+	// Get Mode
+		while (1)
+		{
+			if ( recvmesg[recvcount] == 0 )
+			{
+				parsecount++; 
+				recvcount++;
+				break;
+			}
+
+			parsedmesg[parsecount] = recvmesg[recvcount];
+			parsecount++;
+			recvcount++;
+		}
+
+		memcpy(&parsedmesg[parsecount], space, sizeof(space));
+		parsecount++;
+
+		//write(1, parsedmesg, sizeof(parsedmesg));
+
+	// Get Address from packet
+		memcpy(&parsedmesg[parsecount], from, sizeof(from));
+		parsecount+=strlen(from);
+
+		write(1, parsedmesg, sizeof(parsedmesg));
+
+		if( (clientIP = inet_ntoa(cliaddr.sin_addr)) < 0 )
+			perror("Error converting IP address to string");
+
+		memcpy(&parsedmesg[parsecount], clientIP, sizeof(clientIP));
+
+		write(1, parsedmesg, sizeof(parsedmesg));
+
+}
+*/
 	
 int main(int argc, char **argv)
 {
-
-	// Initialize Socket
 	int sockfd, port;
 	struct sockaddr_in servaddr, cliaddr;
 
@@ -30,6 +156,7 @@ int main(int argc, char **argv)
 	if( (bind(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr))) < 0)
 		perror("bind error");
 
+
 	// Receive and write packets
 
 	int numBytesReceived = 0; 
@@ -39,80 +166,92 @@ int main(int argc, char **argv)
 	bzero(recvmesg, sizeof(recvmesg));
 	bzero(parsedmesg, sizeof(parsedmesg));
 
-	numBytesReceived = recvfrom(sockfd, recvmesg, REQUEST_LENGTH, 0, (struct sockaddr*) &cliaddr, &length);
+	for(;;)
+	{
+		numBytesReceived = recvfrom(sockfd, recvmesg, REQUEST_LENGTH, 0, (struct sockaddr*) &cliaddr, &length);
 
-	// Parse 
+		// Parse Function -- Take string as input buffer, parse from request and put into new buffer to print
 	char RRQ[4] = "RRQ ";
 	char WRQ[4] = "WRQ ";
+	char *filename;
+	char *mode;
 	char *clientIP;
 	char from[5] = "from ";
 	char space[] = " "; 
+	char writeChar;
 	int parsecount = 3;
 	int recvcount;
 
-	// Check for RRQ/WRQ
+	// Check for RRQ
 
 	if ((recvmesg[0] == 0) && (recvmesg[1] == 1))
 		memcpy(parsedmesg, RRQ, sizeof(RRQ));
-	else if ((recvmesg[0] == 0) && (recvmesg[1] == 2))
+	else if ((recvmesg[0] == 0) && (recvmesg[1] == 1))
 		memcpy(parsedmesg, WRQ, sizeof(WRQ));
 	else
 		perror("Not RRQ/WRQ packet");
 
+	//write(1, parsedmesg, strlen(parsedmesg));
+
 	memcpy(&parsedmesg[parsecount], space, sizeof(space));
-	parsecount++;
 
 	// Get FileName
-	recvcount = 2;
-	
-	while (1)
-	{
-		if ( recvmesg[recvcount] == 0 )
-			{parsecount++; recvcount++; break;}
-
-		parsedmesg[parsecount] = recvmesg[recvcount];
 		parsecount++;
-		recvcount++;
-	}
-	memcpy(&parsedmesg[parsecount], space, sizeof(space));
-	parsecount++;
-
-	// Get Mode
-	while (1)
-	{
-		if ( recvmesg[recvcount] == 0 )
+		recvcount = 2;
+		
+		while (1)
 		{
-			parsecount++; 
+			if ( recvmesg[recvcount] == 0 )
+				{parsecount++; recvcount++; break;}
+
+			parsedmesg[parsecount] = recvmesg[recvcount];
+			parsecount++;
 			recvcount++;
-			break;
 		}
 
-		parsedmesg[parsecount] = recvmesg[recvcount];
-		parsecount++;
-		recvcount++;
-	}
 
-	memcpy(&parsedmesg[parsecount], space, sizeof(space));
-	parsecount++;
+		//write(1, parsedmesg, sizeof(parsedmesg));
+		
+		memcpy(&parsedmesg[parsecount], space, sizeof(space));
+		parsecount++;
+
+	// Get Mode
+		while (1)
+		{
+			if ( recvmesg[recvcount] == 0 )
+			{
+				parsecount++; 
+				recvcount++;
+				break;
+			}
+
+			parsedmesg[parsecount] = recvmesg[recvcount];
+			parsecount++;
+			recvcount++;
+		}
+
+		memcpy(&parsedmesg[parsecount], space, sizeof(space));
+		parsecount++;
+
+		//write(1, parsedmesg, sizeof(parsedmesg));
 
 	// Get Address from packet
-	memcpy(&parsedmesg[parsecount], from, sizeof(from));
-	parsecount+=strlen(from);
+		memcpy(&parsedmesg[parsecount], from, sizeof(from));
+		parsecount+=strlen(from);
 
-	write(1, parsedmesg, sizeof(parsedmesg));
+		write(1, parsedmesg, sizeof(parsedmesg));
 
-	int a, b, c, d;
-	unsigned long host;
-	host = ntohl(cliaddr.sin_addr.s_addr);
-	a = host >> 24;
-	b = (host >> 16) & 0xff;
-	c = (host >> 8) & 0xff;
-	d = host & 0xff;
+		int a, b, c, d;
+		unsigned long host;
+		host = ntohl(cliaddr.sin_addr.s_addr);
+		a = host >> 24;
+		b = (host >> 16) & 0xff;
+		c = (host >> 8) & 0xff;
+		d = host & 0xff;
 
-	printf("%d.%d.%d.%d\n", a, b, c, d);
+		printf("%d.%d.%d.%d\n", a, b, c, d);
 
-	// Send Error Code
-	// Make Error Packet
+
 	char sendbuf[REQUEST_LENGTH];
 	int bufferCount = 0;
 	const int zero = 0;
@@ -132,9 +271,14 @@ int main(int argc, char **argv)
 	bufferCount += strlen(FileNotFound);
 	memcpy( &sendbuf[bufferCount], &zero, sizeof(zero));
 
-	// Sending
+	// Write error message to STDOUT
+	write(1, sendbuf, sizeof(sendbuf));
+
 	if( (sendto(sockfd, sendbuf, sizeof(sendbuf), 0, (struct sockaddr*) &cliaddr, sizeof(cliaddr))) < 0 )
 		perror("Error Sending Error Packet");
+
+	}
+// ReceiveAndPrint(sockfd, (struct sockaddr*) &cliaddr, sizeof(cliaddr));
 
 	return 0;	
 }
