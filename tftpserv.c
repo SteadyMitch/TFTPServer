cbce1b19d15573292920eb48514996d66e223cfb
steadymitch <steadymitch@gmail.com>
2013-04-18 10:01:07 -0400

    Iterative Server for RRQ

--- tftpserv.c
+++ tftpserv.c
@@ -13,20 +13,41 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <time.h>
+#include <pthread.h>
 
-int REQUEST_LENGTH = 512;
 
-/* int sendWRQ
-*/
+// Global Declarations
 
-int sendDATA(int sockfd, struct sockaddr_in cliaddr, char *filename)
+int REQUEST_LENGTH = 516;
+
+// Function Definitions
+
+int sendDATA(int sockfd, struct sockaddr_in servaddr, struct sockaddr_in cliaddr, char *filename)
 {
-	char sendbuf[512];
-	int bufferCount = 0, blockCount = 0, filefd = 0;
+
+	int clientfd, numBytesRead, tftpPort = ((int)time(NULL)%65)*1000;
+	struct sockaddr_in TIDaddr;
+
+	if( (clientfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		perror("socket error");
+
+	bzero(&TIDaddr, sizeof(struct sockaddr_in));
+	TIDaddr.sin_family=AF_INET;
+	TIDaddr.sin_addr.s_addr=servaddr.sin_addr.s_addr;
+	TIDaddr.sin_port=htons(tftpPort);
+
+	if( (bind(clientfd, (struct sockaddr*) &TIDaddr, sizeof(TIDaddr))) < 0)
+		perror("bind error");
+
+	char sendbuf[REQUEST_LENGTH];
+	char recvACK[4];
+	socklen_t clilength = sizeof(cliaddr);
+	int bufferCount = 0, blockCount = 1, filefd = 0;
 	const int zero = 0;
 	const int three = 3;
-
-	printf("Enters sendDATA Function and initializes\n");
+	int tensBlockDigit;
+	int onesBlockDigit;
 
 	// Check if file exists, if not send ERROR packet
 	if( (filefd = open(filename, 0)) < 0  )
@@ -36,21 +57,39 @@ int sendDATA(int sockfd, struct sockaddr_in cliaddr, char *filename)
 	{
 		bzero(sendbuf, sizeof(sendbuf));
 
+		// Op Code for DATA
 		memcpy( &sendbuf[bufferCount], &zero, sizeof(zero));
 		bufferCount++;
 		memcpy( &sendbuf[bufferCount], &three, sizeof(three));
 		bufferCount++;
+
+		memcpy( &sendbuf[bufferCount], &zero, sizeof(zero));
+		bufferCount++;
 		memcpy( &sendbuf[bufferCount], &blockCount, sizeof(blockCount));
-		bufferCount+=2;
+		bufferCount++;
 
-		read( filefd, &sendbuf[bufferCount], 508);
 
-		//sendto(sockfd, sendbuf, sizeof(sendbuf), 0);
-		write(1, sendbuf, sizeof(sendbuf));
-
-		if( sizeof(sendbuf) < 512 )
+		// Read file into packet and send, break if last data packet
+		if( (numBytesRead = read( filefd, &sendbuf[bufferCount], 512)) < 512)
+		{
+			sendto(clientfd, sendbuf, numBytesRead + 4, 0, (struct sockaddr*) &cliaddr, sizeof(cliaddr));
+			
+			// Wait for ACK packet, send again if no response
+			if (!recvfrom(clientfd, recvACK, sizeof(recvACK), 0, (struct sockaddr*) &cliaddr, &clilength))
+				sendto(clientfd, sendbuf, sizeof(sendbuf), 0, (struct sockaddr*) &cliaddr, sizeof(cliaddr));	
 			break;
+		}
+
+		sendto(clientfd, sendbuf, sizeof(sendbuf), 0, (struct sockaddr*) &cliaddr, sizeof(cliaddr));
+
+		// Wait for ACK packet, send again if no response
+		if (!recvfrom(clientfd, recvACK, sizeof(recvACK), 0, (struct sockaddr*) &cliaddr, &clilength))
+			sendto(clientfd, sendbuf, sizeof(sendbuf), 0, (struct sockaddr*) &cliaddr, sizeof(cliaddr));		
+		// Increment block count and reset buffer count
+		blockCount++;
+		bufferCount = 0;
 	}
+
 }
 
 /*int sendACK(int sockfd, struct sockaddr_in cliaddr)
@@ -89,14 +128,11 @@ int sendERROR(int sockfd, struct sockaddr_in cliaddr)
 	bufferCount += strlen(FileNotFound);
 	memcpy( &sendbuf[bufferCount], &zero, sizeof(zero));
 
-	// Write error message to STDOUT
-	//write(1, sendbuf, sizeof(sendbuf));
-
 	if( (sendto(sockfd, sendbuf, sizeof(sendbuf), 0, (struct sockaddr*) &cliaddr, sizeof(cliaddr))) < 0 )
 		perror("Error Sending Error Packet");
 }
 
-	
+
 
 void ParseRequest(char *recvmesg, int numBytesReceived, struct sockaddr_in cliaddr, int *requestBit, char* Filename, char* Mode, char* ClientIP)
 {
@@ -123,7 +159,7 @@ void ParseRequest(char *recvmesg, int numBytesReceived, struct sockaddr_in cliad
 			memcpy(parsedmesg, RRQ, sizeof(RRQ));
 			*requestBit = 1;
 		}
-	else if ((recvmesg[0] == 0) && (recvmesg[1] == 1))
+	else if ((recvmesg[0] == 0) && (recvmesg[1] == 2))
 		{
 			memcpy(parsedmesg, WRQ, sizeof(WRQ));
 			*requestBit = 2;
@@ -137,7 +173,7 @@ void ParseRequest(char *recvmesg, int numBytesReceived, struct sockaddr_in cliad
 		parsecount++;
 		recvcount = 2;
 
-	memcpy(filename, &recvmesg[recvcount], sizeof(filename));
+		memcpy(filename, &recvmesg[recvcount], sizeof(filename));
 	
 		while (1)
 		{
@@ -153,8 +189,6 @@ void ParseRequest(char *recvmesg, int numBytesReceived, struct sockaddr_in cliad
 			parsecount++;
 			recvcount++;
 		}
-
-		//write(1, parsedmesg, sizeof(parsedmesg));
 
 		memcpy(&parsedmesg[parsecount], space, sizeof(space));
 		parsecount++;
@@ -206,7 +240,7 @@ void ParseRequest(char *recvmesg, int numBytesReceived, struct sockaddr_in cliad
 int main(int argc, char **argv)
 {
 	int sockfd, tftpPort;
-	struct sockaddr_in servaddr, cliaddr;
+	struct sockaddr_in servaddr;
 
 	if( argc != 2 )
 		perror("Usage: ./tftpserv <port number>\n");
@@ -224,38 +258,43 @@ int main(int argc, char **argv)
 	if( (bind(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr))) < 0)
 		perror("bind error");
 
-
 	// Receive and write packets
 
 	int numBytesReceived = 0; 
 	char recvmesg[REQUEST_LENGTH];
 	char parsedmesg[REQUEST_LENGTH];
+	struct sockaddr_in cliaddr;
 	socklen_t length = sizeof(cliaddr);
 	bzero(recvmesg, sizeof(recvmesg));
 	bzero(parsedmesg, sizeof(parsedmesg));
 
 	for(;;)
 	{
-	numBytesReceived = recvfrom(sockfd, recvmesg, REQUEST_LENGTH, 0, (struct sockaddr*) &cliaddr, &length);
+		numBytesReceived = recvfrom(sockfd, recvmesg, REQUEST_LENGTH, 0, (struct sockaddr*) &cliaddr, &length);
 
-	// Parse Function -- Take string as input buffer, parse request and put into new buffer to print.  Extracts filename, TFTP mode, client IP address
-	int requestBit;
-	char filename[50];
-	char mode[10];
-	char clientIP[16];
-	ParseRequest(recvmesg, numBytesReceived, cliaddr, &requestBit, filename, mode, clientIP);
+		// Parse Function -- Take string as input buffer, parse request and put into new buffer to print.  Extracts filename, TFTP mode, client IP address
+		int requestBit;
+		char filename[50];
+		char mode[10];
+		char clientIP[16];
+		ParseRequest(recvmesg, numBytesReceived, cliaddr, &requestBit, filename, mode, clientIP);
 
 
-	// RRQ
-	if(requestBit == 1)
-		sendDATA(sockfd, cliaddr, filename);
+		switch ( requestBit )
+		{
+			case 1:	// RRQ
+				sendDATA(sockfd, servaddr, cliaddr, filename);
+				break;
 
-	// WRQ
-	if(requestBit == 2)
-		{}// writeDATA
+			case 2:	// WRQ
+				// write
+				break;
 
-	sendERROR(sockfd, cliaddr);
-
-	}
-	return 0;	
+			default://Send ERROR packet
+				sendERROR(sockfd, cliaddr);
+				break;
+		
+		}
+	}	
+    return 0;
 }
